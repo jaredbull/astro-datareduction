6a3c1ae5470c9a43a8418096803d9b723d4f3e18
jaredbull <135657577+jaredbull@users.noreply.github.com>
2023-06-29 21:25:22 -0500

    Replaced old version

--- landolt.py
+++ landolt.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 
-# In[7]:
+# In[3]:
 
 
 # copy set of standard stars from database into a .txt file before running script
 # default delimiter is whitespace, keep this in mind when extracting desired columns from .txt
+# pandas would likely work better in general for this
     
 V_BminusV = np.loadtxt('E:/work/landolt_104.txt',dtype='float',usecols=(9,10))
 
@@ -33,7 +37,7 @@ V_BminusV
 V_BminusV.shape
 
 
-# In[16]:
+# In[4]:
 
 
 # plotting V as a function of B-V for set of standard stars
@@ -44,7 +48,7 @@ plt.xlabel('B-V')
 plt.ylabel('V')
 
 
-# In[17]:
+# In[5]:
 
 
 # same process for other sets of stars taken from database
@@ -54,4 +58,35 @@ plt.scatter(V_BminusV[:,1],V_BminusV[:,0])
 plt.gca().invert_yaxis()
 plt.xlabel('B-V')
 plt.ylabel('V')
+
+
+# In[2]:
+
+
+# reading in data for RA and DEC for all stars
+# 'pd.read_table()' works best for this case since columns are separated by tabbed spaces
+
+landolt_radec = pd.read_table('E:/work/landolt_110.txt',header=None,usecols=[0,2,3])
+landolt_radec
+
+
+# In[7]:
+
+
+# making lists of RA and DEC for all stars in set
+
+RA = landolt_radec[2]
+DEC = landolt_radec[3]
+c = SkyCoord(RA,DEC,unit=(u.hourangle, u.deg))
+c
+
+
+# In[26]:
+
+
+plt.scatter(c.ra.degree,c.dec.degree)
+plt.gca().invert_xaxis()
+plt.grid()
+plt.xlabel('RA (Deg)')
+plt.ylabel('DEC (Deg)')
 
